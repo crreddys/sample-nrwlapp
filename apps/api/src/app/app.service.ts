f4428e8c3dda6e5e323ea35ebee84dbc4e4d024b
Ramnarayan Chinavelaka <ramnarayan.chinavelaka@fmglobal.com>
2020-04-11 23:20:50 -0400

    Moving the Interface to shared library

--- apps/api/src/app/app.service.ts
+++ apps/api/src/app/app.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@nestjs/common';
-
-interface Todo {
-  title: string;
-}
+import { Todo } from '@sample-nrwlapp/data';
 
 @Injectable()
 export class AppService {
--- apps/mysite/src/app/app.component.ts
+++ apps/mysite/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-interface Todo {
-  title: string;
-}
+import { Todo } from '@sample-nrwlapp/data';
 
 @Component({
   selector: 'sample-nrwlapp-root',
